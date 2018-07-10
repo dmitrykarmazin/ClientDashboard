5fc13079f0b0c7d92d6417fa9168afbed480c2be
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-07-10 19:06:19 +0300

    sintax fix

--- src/app/services/clients-data.service.ts
+++ src/app/services/clients-data.service.ts
@@ -6,10 +6,10 @@ import { Client } from "../model/Client";
 
 @Injectable()
 export class ClientsDataService {
-  private Url: string = "../../assets/clients.json";
+  private Url: string = "../assets/clients.json";
   constructor(private http: HttpClient) {}
 
   getClientsData(): Observable<Client[]> {
-    return this.http.get<Client[]>("../assets/clients.json");
+    return this.http.get<Client[]>(this.Url);
   }
 }
