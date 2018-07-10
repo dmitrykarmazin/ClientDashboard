c8aab7fcb757e6a2c0930458a4248f9535a72e07
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-07-10 10:25:13 +0300

    add search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+/*material*/
+import { MatSidenavModule } from "@angular/material";
+import { MatInputModule } from "@angular/material/input";
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule, 
+    MatSidenavModule, 
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
