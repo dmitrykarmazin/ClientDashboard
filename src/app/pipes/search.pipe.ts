a39b80340bb75db7c5de4b683bac8fd24239fa7d
dmitrykarmazin <dmitriy.karmazin92@gmail.com>
2018-07-12 09:35:37 +0300

    upd pipe, search by all fields

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -10,9 +10,11 @@ export class SearchPipe implements PipeTransform {
     if (!items) { return []; }
     if (!query) { return items; }
     query = query.toLowerCase();
-    return items.filter( item => {
-      const str = item.general.firstName.toLowerCase();
-      return str.includes(query);
+    return items.filter(item => {
+      return Object.values(item)
+        .map(v => Object.values(v))
+        .reduce((a, b) => a.concat(b))
+        .some((i: string) => i.toLowerCase().includes(query));
     });
   }
 
